76cf6a34aced41102c19d85ec3bae7836e349449
unknown <joshkamrass@gmail.com>
2021-06-12 20:01:09 -0400

    adds near mongoose query for explore endpoint

--- pages/api/explore/index.js
+++ pages/api/explore/index.js
@@ -1,9 +1,22 @@
 import dbConnect from "../../../utils/dbConnect";
 import Test from "../../../models/Test";
+import Specimen from "../../../models/Specimen";
 
 export default async (req, res) => {
-
   await dbConnect();
-  const allTests = await Test.find({}).exec();
-  res.status(200).json(allTests);
+  const searchLocation = [-78.92876857, 36.01385727];
+  const nearbySpecimens = await Specimen.find({
+    location: {
+      $near: {
+        $geometry: {
+          type: "Point",
+          coordinates: searchLocation
+        },
+        $maxDistance: 3000
+      }
+    }
+    },
+    "species"
+  )
+  res.status(200).json(nearbySpecimens);
 }
