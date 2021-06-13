935e21d668957aecef43a0b0aae93433e861e4ab
unknown <joshkamrass@gmail.com>
2021-06-12 21:18:25 -0400

    refactors explore endpoint as aggregation pipeline

--- pages/api/explore/index.js
+++ pages/api/explore/index.js
@@ -5,6 +5,7 @@ import Specimen from "../../../models/Specimen";
 export default async (req, res) => {
   await dbConnect();
   const searchLocation = [-78.92876857, 36.01385727];
+  const radiusOfSearchInMiles = 2;
   const nearbySpecimens = await Specimen.find({
     location: {
       $near: {
@@ -18,5 +19,16 @@ export default async (req, res) => {
     },
     "species"
   )
-  res.status(200).json(nearbySpecimens);
+  const nearbySpecies = await Specimen.aggregate([
+    {$match: {
+      location: {
+        $geoWithin: {
+          // converts the distance from miles to radians by dividing by the approximate equatorial radius of the earth, 3963.2 miles
+          $centerSphere: [searchLocation, radiusOfSearchInMiles/3963.2]
+        }
+      }
+    }},
+    {$group: {_id: "$species", numberOfSightings: {$sum: "$numberOfSightings"}}}
+  ])
+  res.status(200).json(nearbySpecies);
 }
