74ff5f309a1e2f3f0ad27e0ba3bd0c4039885c2b
unknown <joshkamrass@gmail.com>
2021-06-14 09:47:38 -0400

    refactors map marker to use fa leaf icon

--- components/specimenMapMarker.js
+++ components/specimenMapMarker.js
@@ -1,7 +1,17 @@
+import { faLeaf } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
 export default function SpecimenMapMarker ({text}) {
   return (
-    <div>
-      {text}
+    <div style={markerStyle}>
+      <FontAwesomeIcon icon={faLeaf} size="2x" color="green"/>
     </div>
   )
+}
+
+// sets the marker to center itself on the coordinates, rather than the default of its top left corner being at the coordinates
+const markerStyle = {
+  position: 'absolute',
+  transform: 'translate(-50%, -50%)'
 }
