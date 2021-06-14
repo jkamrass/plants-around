46739bbb54aad924e6ffd7f96ce66b5483070954
unknown <joshkamrass@gmail.com>
2021-06-14 14:52:30 -0400

    puts image on side of explore cards

--- components/nearbySpeciesList.js
+++ components/nearbySpeciesList.js
@@ -6,20 +6,24 @@ export default function NearbySpeciesList ({nearbySpecies}) {
     <div className="row">
       {nearbySpecies.map((aNearbySpecies) => {
         return (
-          <div className="col-md-2" key={aNearbySpecies._id._id}>
+          <div className="col-sm-3" key={aNearbySpecies._id._id}>
             <div className="card">
               <Link href={`/explore/${aNearbySpecies._id._id}`}>
-                <div>
-                  <Image
-                    src={`${aNearbySpecies._id.thumbnail}`}
-                    height={250}
-                    width={250}
-                    layout="responsive"
-                    className="card-img-top"
-                    alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{aNearbySpecies._id.name}</h5>
-                    Number of Sightings in your area: {aNearbySpecies.numberOfSightings}
+                <div className="row g-0">
+                  <div className="col-6">
+                    <Image
+                      src={`${aNearbySpecies._id.thumbnail}`}
+                      height={250}
+                      width={250}
+                      layout="responsive"
+                      className="card-img-top img-thumbnail"
+                      alt="..." />
+                  </div>
+                  <div className="col-6 text-start">
+                    <div className="card-body">
+                      <h6 className="card-title">{aNearbySpecies._id.name}</h6>
+                      Sightings: {aNearbySpecies.numberOfSightings}
+                    </div>
                   </div>
                 </div>
               </Link>
