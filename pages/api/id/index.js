5e6feffd19bb72a96a07abe7d26493f5bd439541
unknown <joshkamrass@gmail.com>
2021-06-15 09:16:51 -0400

    adds plant net verification object

--- pages/api/id/index.js
+++ pages/api/id/index.js
@@ -5,26 +5,35 @@ import dbConnect from "../../../utils/dbConnect";
 
 export default async (req, res) => {
   if (req.method === "POST") {
-    // Check to make sure species id is valid
     await dbConnect();
     const speciesForSighting = await Species.findById(req.body.species);
+    // Request validation
+    // Check to make sure species id is valid
     if (!speciesForSighting) {
       return res.status(404).send("No species with that id found");
     }
-    if (!req.body.images) {
-      return res.status(400).send("Must include images");
-    }
+    // // Check to make sure images and plant organs are valid
+    // if (!req.body.images) {
+    //   return res.status(400).send("Must include images");
+    // }
+    // Check to make sure the location is valid
+
     // Create the new document for this sighting
-    const newSighting = new Sighting();
-    newSighting.species = speciesForSighting;
-    newSighting.location = {
-      type: "Point",
-      coordinates: [req.body.location.longitude, req.body.location.latitude]
-    }
+    // const newSighting = new Sighting();
+    // newSighting.species = speciesForSighting;
+    // newSighting.location = {
+    //   type: "Point",
+    //   coordinates: [req.body.location.longitude, req.body.location.latitude]
+    // }
     // Add images to sighting
+
+    // Test for plant net:
+    const imagesToId = [{imageUrl: "https://www.irishtimes.com/polopoly_fs/1.3997665.1566828860!/image/image.jpg", organ: "leaf"}]
     // Send the results to plant net to identify
-    const plantNetIdResponse = await getPlantNetIdResults(req.body.images);
-    console.log(plantNetIdResponse);
+    const plantNetIdResponse = await getPlantNetIdResults(imagesToId);
+    const speciesMatch = plantNetIdResponse.data.results.find((possibleSpeciesMatch) => possibleSpeciesMatch.species.scientificNameWithoutAuthor === speciesForSighting.scientificName)
+    const plantNetVerification = {source: "Plant Net", matchScore: speciesMatch ? speciesMatch.score : 0};
+    //plantNetVerification.matchScore = speciesMatch ? speciesMatch.score : 0;
     // Check if plant net id matches with the species
 
     // Respond to the user either that their sighting is verified or it has been sent to be seen by a moderator
@@ -33,7 +42,7 @@ export default async (req, res) => {
     // newSighting.verified = "TRUE";
     // newSighting.verifications = [{source: "falling fruit"}]
     // newSighting.us
-    res.status(200).json("It worked");
+    return res.status(200).json("It worked");
   }
   res.status(405).send("Must be a POST request");
 }
