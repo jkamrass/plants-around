4bc2631e0cc6a019ac5484939c1f3b661b912a33
unknown <joshkamrass@gmail.com>
2021-06-15 10:48:26 -0400

    adds link for id page to navbar and sets navbar to collapse on selection

--- components/navbar.js
+++ components/navbar.js
@@ -3,14 +3,14 @@ import Link from 'next/link';
 
 export default function NavbarMain () {
   return (
-    <Navbar bg="light" expand="lg">
-      <Link href="/" passHref><Navbar.Brand href="#home">Plants Around</Navbar.Brand></Link>
+    <Navbar bg="light" expand="lg" collapseOnSelect>
+      <Link href="/" passHref><Navbar.Brand>Plants Around</Navbar.Brand></Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Challenge</Nav.Link>
           <Link href="/explore" passHref><Nav.Link>Explore</Nav.Link></Link>
-          <Nav.Link href="#link">Id</Nav.Link>
+          <Link href="/id" passHref><Nav.Link>Id</Nav.Link></Link>
           <Nav.Link href="#link">Login/Signup</Nav.Link>
         </Nav>
       </Navbar.Collapse>
