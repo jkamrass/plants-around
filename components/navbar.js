36881461c193597f7529ae0d8556a51cde2559a8
unknown <joshkamrass@gmail.com>
2021-06-16 21:24:48 -0400

    removes client side next auth and renames navbar section

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 import { Navbar, Nav } from "react-bootstrap";
 import Link from 'next/link';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera, faCheckSquare, faMap } from "@fortawesome/free-solid-svg-icons";
+import { faCamera, faSeedling, faMap } from "@fortawesome/free-solid-svg-icons";
 import { signIn } from "next-auth/client";
 
 export default function NavbarMain () {
@@ -11,11 +11,10 @@ export default function NavbarMain () {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Challenge</Nav.Link>
-          <Link href="/explore" passHref><Nav.Link><FontAwesomeIcon icon={faMap} /> Verify</Nav.Link></Link>
+          <Link href="/explore" passHref><Nav.Link><FontAwesomeIcon icon={faMap} /> Explore</Nav.Link></Link>
           <Link href="/id" passHref><Nav.Link><FontAwesomeIcon icon={faCamera} /> Id</Nav.Link></Link>
-          <Link href="/verify" passHref><Nav.Link><FontAwesomeIcon icon={faCheckSquare} /> Verify</Nav.Link></Link>
-          <Nav.Link onClick={() => signIn()}>Login/Signup</Nav.Link>
+          <Link href="/verify" passHref><Nav.Link><FontAwesomeIcon icon={faSeedling} /> Sightings</Nav.Link></Link>
+          <Nav.Link>Login/Signup</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import NavbarMain from '../components/navbar'
 import '../styles/globals.css'
-import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
   return (
     <> 
-      <Provider session={pageProps.session}>
-        <NavbarMain />
-        <Component {...pageProps} />
-      </Provider>
+      <NavbarMain />
+      <Component {...pageProps} />
     </>
   )
 }
